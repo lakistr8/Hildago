656d950ba8297d94b115c05e3628348bc3d9ab80
Lazar <lakiando@outlook.com>
2016-12-04 13:34:37 +0100

    added 2 cocoa touch files
    one for table view controller
    one for table view cell

---
+++ Hildago/ContactsCell.swift
@@ -0,0 +1,24 @@
+//
+//  ContactsCell.swift
+//  Hildago
+//
+//  Created by iosakademija on 12/4/16.
+//  Copyright © 2016 iosakademija. All rights reserved.
+//
+
+import UIKit
+
+class ContactsCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Hildago/ContactsController.swift
@@ -0,0 +1,43 @@
+//
+//  ContactsController.swift
+//  Hildago
+//
+//  Created by iosakademija on 12/4/16.
+//  Copyright © 2016 iosakademija. All rights reserved.
+//
+
+import UIKit
+
+class ContactsController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ContactsCell", for: indexPath) as! ContactsCell
+
+
+        return cell
+    }
+    
+}

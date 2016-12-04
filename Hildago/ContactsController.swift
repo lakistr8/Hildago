af005f6d364038a005db8be977bf897bf46904ee
Lazar <lakiando@outlook.com>
2016-12-04 13:40:25 +0100

    beautified ContactsController.swift

--- Hildago/ContactsController.swift
+++ Hildago/ContactsController.swift
@@ -8,35 +8,41 @@
 
 import UIKit
 
+protocol ContactsControllerDelegate : class {
+    
+    func contact(controller : ContactsController, didSelect contact: String )
+}
+
+
 class ContactsController: UITableViewController {
+    
+    weak var delegate : ContactsControllerDelegate? = nil
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
+}
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+extension ContactsController {
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return 10
     }
-
+    
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ContactsCell", for: indexPath) as! ContactsCell
-
-
+        
+        
         return cell
     }
     

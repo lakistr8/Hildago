0d19efe7d060906dcf9124fd56022d3b79cf6b68
Lazar <lakiando@outlook.com>
2016-12-04 12:32:57 +0100

    added new file cocoa touch sub class from view controller

---
+++ Hildago/HildagoController.swift
@@ -0,0 +1,18 @@
+//
+//  HildagoController.swift
+//  Hildago
+//
+//  Created by iosakademija on 12/4/16.
+//  Copyright © 2016 iosakademija. All rights reserved.
+//
+
+import UIKit
+
+class HildagoController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
